c42f42e5194f7e0a5bc9e3249c4f55361396bdae
Nicole Cornehl Lima <nclima@Nicoles-MacBook-Air.local>
2026-08-26 14:49:54 -0500

    Memes

--- data/content.js
+++ data/content.js
@@ -14,7 +14,7 @@ window.FRWD_CONTENT = {
     },
     about: {
       label: "About Me",
-      title: "Tech x Culture x Strategy",
+      title: "Tech x\nCulture x\nStrategy",
       body:
         "FRWD Concepts is a concept portfolio by Nicole Cornehl Lima. I just graduated from the University of Michigan. Wohoooo! My major was Computer Science in the College of Engineering.\n\nI'm also a serial explorer. Currently at 48 countries so far! And lastly, I’m online a lot... I just have such a love for things that feel Gen Z and relevant.",
     },
